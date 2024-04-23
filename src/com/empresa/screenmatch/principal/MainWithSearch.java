9dc1f0fe33377fd89cec73beccfc77d5b7f1fb63
Bernal30 <abrn7311@gmail.com>
2024-04-22 22:53:56 -0600

    wip - Adding dinamic API requests

--- src/com/empresa/screenmatch/principal/MainWithSearch.java
+++ src/com/empresa/screenmatch/principal/MainWithSearch.java
@@ -5,16 +5,24 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainWithSearch {
     //throws lanza explícitamente una excepción para errores con protocolos de internet
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        Scanner reading = new Scanner(System.in);
+
+        System.out.print("Escriba el nombre de una pelicula: ");
+        String userRequest = reading.next();
+
+        String dinamicRequest = "https://www.omdbapi.com/?t="+userRequest+"&apikey=72bc7832";
+
         //creamos una request a un servidor mediante HTTP
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 //se coloca el link de la API request
-                .uri(URI.create("https://www.omdbapi.com/?apikey=72bc7832&t=Blade+Runner+2049"))
+                .uri(URI.create(dinamicRequest))
                 .build();
 
         //tenemos que resivir los datos del srevidor
